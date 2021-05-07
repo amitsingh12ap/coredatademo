eb61c6bda27a2d3c2e856b177005ed5c8f8894fb
Amit Kr Singh <amit1.singh@africa.airtel.com>
2021-05-07 16:47:01 +0530

    Initial Commit

---
+++ CoreDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by 13216146 on 07/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
